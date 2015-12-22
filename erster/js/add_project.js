29b347fd2653a5dbb89ba4b83eb3ba34cb551ffd
Anna Schwarz <anna.ryzhova48@gmail.com>
2015-12-22 20:44:57 +0300

    done qtip for modal window

--- erster/js/add_project.js
+++ erster/js/add_project.js
@@ -9,63 +9,78 @@
 			//прослушка событий...
 			$('#add-new').on('click', _showModal); //открыть мод. окно
 			$('#add-form-modal').on('submit', _addProject); //добавить проект 
-		//	$('#fileupload').on('change', _changefileUpload); // добавление файла в #fileupload 
+			$('#fileupload').on('change', _changefileUpload); // добавление файла в #fileupload 
 	};
 
 	var _showModal = function(ev){
 		console.log('Answer');
 		ev.preventDefault();
 	//	$('#new-proj-popup').bPopup();
-	var divPop = $('#new-proj-popup'),
+	var divPop = $('.new-proj-popup'),
 		form = divPop.find('.form-modal');
-		divPop.bPopup();
+		divPop.bPopup(
+			{
+			positionStyle: 'fixed',
+			onClose : function () {
+			console.log('сброс'); // функция из документации bPopup - очистка формы
+				// form.find('.server-message').text('').hide(); // очищаем текст формы и прячем её
+				this.find('.form-modal').trigger("reset"); // сбрасываем форму
+			}
+			});
 	};
+	var _changefileUpload = function () {
+		var input = $(this), 
+			name = input[0].files[0].name; 
+			name = _getNameFromPath(input.val()); 
 
+		$('#filename')
+			.val(name)
+			.trigger('hideTooltip') 
+			.removeClass('has-error'); 
+	};
+		var _getNameFromPath = function (path) {
+		return path.replace(/\\/g, '/').replace(/.*\//, '');
+	};
 	var _addProject = function(ev){
 		ev.preventDefault();
 
-		var form = $(this), // взять "этот" элемент, т.е. в _addProject
+		var form = $(this), 
 			url = $(this).attr('action'),
-			url = 'add_project.php', // файл, к которому будем обращаться при отправке формы
-			myServerGiveMeAnAnswer = _ajaxForm(form, url); // сюда будем передавать ответ из ajaxForm		
+			url = 'add_project.php', 
+			myServerGiveMeAnAnswer = _ajaxForm(form, url); 		
 
-	if (myServerGiveMeAnAnswer) { // Не выполнять код ниже, если форма не проходит валидацию - был ли запрос на сервер
-		myServerGiveMeAnAnswer.done(function(ans) { // сюда будет приходить ответ
+	if (myServerGiveMeAnAnswer) { 
+		myServerGiveMeAnAnswer.done(function(ans) { 
 			// console.log(ans);
 
-			// обработка ошибок при добалвении проекта
 			var successBox = form.find('.success-message'),
 				errorBox = form.find('.error-message');
 
 			if(ans.status === 'OK'){
 				// console.log(ans.text);
 				errorBox.hide();
-				successBox.text(ans.text).show(); // если всё ок, то вставлять текст ок
+				successBox.text(ans.text).show(); 
 			}else{
 				// console.log(ans.text);
 				successBox.hide();
-				errorBox.text(ans.text).show(); // если всё не ок, то вставлять в блок .error-me текст ошибки
+				errorBox.text(ans.text).show(); 
 			}
 		});
 		}
 	};
 
-	var _ajaxForm = function (form, url) { // добавляем универсальную ajax-функцию
+	var _ajaxForm = function (form, url) { 
 
-		// 1. Проверить форму
-		// 2. Собрать данные из формы
-		// 3. Вернуть ответ с сервера
+		if (!validation.validateForm(form)) return false;  
+		var data = form.serialize(); 
 
-		if (!validation.validateForm(form)) return false;  // Возвращает false, если не проходит валидацию
-		var data = form.serialize(); // собираем данные из формы в объект data
-
-		var result = $.ajax({ // результат запроса на сервер помещаем в переменную и далее эту переменную возвращаем
-			url: url, // здесь будет url сайта
+		var result = $.ajax({ 
+			url: url, 
 			type: 'POST',
 			dataType: 'json',
 			data: data,
 		}).fail( function(ans) {
-			// console.log('Проблемы в PHP');
+			 console.log('Проблемы в PHP');
 			form.find('.error-message').text('На сервере произошла ошибка').show();
 		});
 
